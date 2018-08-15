ce6d9a57615ffe29db1b4fc41ea2f7ba16c40c3c
Brett Markowitz <batting1000@comcast.net>
2018-08-14 22:16:41 -0400

    Initial main menu setup + license file

---
+++ weather.py
@@ -0,0 +1,12 @@
+""" This is the main entry point for the weather API command line interface. Displays the main menu to allow access to all functionality."""
+
+try:
+    import requests
+except ImportError:
+    print('Import error')
+
+def weather():
+    pass
+
+if __name__ == '__main__':
+    weather()
